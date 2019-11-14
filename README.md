7d7122d34454b48a2edf807bf0f3bce95e48acb1
xiaodong.li <xiaodong.li@asiainnovations.com>
2019-11-14 17:15:47 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,20 @@
+# Prometheus Docker-Compose
+
+### Command
+
+```
+
+#启动容器：
+docker-compose -f /data/work/prometheus-monitor/config/docker-compose-monitor.yml up -d
+#删除容器：
+docker-compose -f /data/work/prometheus-monitor/config/docker-compose-monitor.yml down
+#重启容器：
+docker restart id
+
+```
+
+### 引用
+- 作者：Java十一
+- 来源：掘金
+- 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+- [🔗](https://juejin.im/post/5c9dc0b06fb9a070ae3da6e7)
---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  deploy : {
+    production : {
+      production: {
+        'user': 'root',
+        'host': '47.104.216.30',
+        'ref': 'origin/master',
+        'repo': 'git@github.com:NextZeus/prometheus-monitor.git',
+        'path': '/data/work/prometheus-monitor',
+        'pre-setup': "yum install git -y;",
+        'post-setup': "ls -la",
+        'pre-deploy-local': "pwd; echo 'this is a local command' ",
+        'pre-deploy': 'ls -la;',
+        'post-deploy': 'git pull origin master;git log -n 2;', 
+        'test': 'docker ps;'
+      }
+    }
+  }
+};
