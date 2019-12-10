bb4220ae62033b2088113a6bdea4a49353dab9c7
老顽童 <875697126@qq.com>
2019-12-10 15:49:41 +0800

    update compose grafana 持久化

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,12 @@ module.exports = {
       'user': 'root',
       'host': '47.104.216.30',
       'ref': 'origin/master',
-      'repo': 'git@github.com:NextZeus/prometheus-monitor.git',
+      'repo': 'git@github.com:NextZeus/pm2-prometheus-monitor-compose.git',
       'path': '/data/work/prometheus-monitor',
       'pre-setup': "yum install git -y;",
       'post-setup': "ls -la",
       'pre-deploy-local': "pwd; echo 'this is a local command' ",
-      'pre-deploy': 'pwd;ls -la;',
+      'pre-deploy': 'pwd;mkdir -p grafana;ls -la;',
       'post-deploy': 'git pull origin master;git log -n 2;',
       'test': 'pwd;docker ps;'
     }
